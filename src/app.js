9f80dd637dc5d96934124efb45a0dfdfbd6dd67a
Andrey Bakin <andybulkin@yandex.ru>
2024-07-14 22:32:23 +0300

    доработана логика

--- src/app.js
+++ src/app.js
@@ -1,24 +1,46 @@
-const http = require('http');
-const getUsers = require('./modules/users');
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const getUsers = require("./modules/users");
 
-const server = http.createServer((request, response) =>{
-    if (request.url === '/users') {
-        response.status = 200;
-        response.statusMessage = "OK";
-        response.header = "Content-Type: application/json"
-        response.write(getUsers());
-        response.end();
+const hostname = "127.0.0.1";
+const port = 3003;
 
-        return;
+const server = http.createServer((request, response) => {
+  const userName = url.parse(request.url, true).query;
+
+  if (userName.hello !== undefined) {
+    if (userName.hello) {
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "text/plain");
+      response.end(`Hello, ${userName.hello}`);
+    } else {
+      response.statusCode = 400;
+      response.setHeader("Content-Type", "text/plain");
+      response.end("Enter a name");
     }
-
-    response.status = 200;
+  } else if (userName.users !== undefined) {
+    fs.readFile("src/data/users.json", (error, getUsers) => {
+      if (error) {
+        response.statusCode = 500;
+        response.end();
+      } else {
+        response.statusCode = 200;
+        response.setHeader("Content-Type", "application/json");
+        response.end(getUsers);
+      }
+    });
+  } else if (Object.keys(userName).length === 0) {
+    response.statusCode = 200;
     response.statusMessage = "OK";
-    response.header = "Content-Type: text/plain"
-    response.write("Hello, world");
+    response.setHeader("Content-Type", "text/plain");
+    response.end("Hello, world");
+  } else {
+    response.statusCode = 500;
     response.end();
+  }
 });
 
-server.listen(3003, () => {
-    console.log("Сервер запущен по адресу http://127.0.0.1:3003")
+server.listen(port, hostname, () => {
+  console.log(`Сервер запущен по адресу http://${hostname}:${port}`);
 });
